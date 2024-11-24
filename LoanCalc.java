aee5f83dd9634d949ed92dc13a7a26a6d7d68793
Simon Levy <simon.levy01@post.runi.ac.il>
2024-11-24 15:59:59 +0200

    checking assignment 2

--- LoanCalc.java
+++ LoanCalc.java
@@ -29,8 +29,8 @@ public class LoanCalc {
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
 		double total = loan;
-		for (int i=0 ; i<n ; i++){
-			total = (total-payment)*(1+(rate/100.0));
+		for (int i=0 ; i<n ; i++){ // calculate total after amount of payments
+			total = (total-payment)*(1+(rate/100.0)); //iterate on new total 
 		}
 		return total;
 	}
@@ -41,11 +41,11 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		double g=loan/n;
-		iterationCounter = 0;
-		while (endBalance(loan, rate, n, g)>0){
-			g=g+epsilon;
-			iterationCounter += 1;
+		double g=loan/n; // initial guess
+		iterationCounter = 0; //reset counter
+		while (endBalance(loan, rate, n, g)>=epsilon){ // continue until within accuracy range
+			g += 0.01; //increment guess
+			iterationCounter++; //increment counter
 		}
 		return g;
     }
@@ -56,7 +56,21 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+        double L = loan/n; //lower bound
+		double H = loan; // upper bound
+		double g = (L+H)/2; //initial guess
+		double balance = endBalance(loan, rate, n, g); 
+		iterationCounter = 0; // reset step counter
+		while (Math.abs(balance)>=epsilon){ // continue until within accuracy range
+			if (balance >0){ //if balance is positive, payment is too low
+				L=g;
+			} else { //if balance is negative, payment is too high
+				H=g;
+			}
+			g = (L+H)/2; //new guess
+			balance = endBalance(loan, rate, n, g); //recaclulate balance
+			iterationCounter++; //increment counter
+		}
+		return g;
     }
 }
